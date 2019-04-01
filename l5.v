a55cf1f2e82c605e797fbf5e3cbda8695f2a6225
Kamirus <listopadkamil@gmail.com>
2019-04-01 20:44:48 +0200

    l5 insert

--- l5.v
+++ l5.v
@@ -1,3 +1,5 @@
+Require Import PeanoNat.
+
 (*** Zadanie 1 - 3p *)
 (* Rozważmy typ list indeksowanych długością *)
 
@@ -121,14 +123,174 @@ Qed.
 
 (* 1. Zdefiniuj predykat [sorted] spełniony dla list posortowanych rosnąco. *)
 
-(* sorted : list nat -> Prop *)
+Inductive sorted : list nat -> Prop :=
+| sorted_nil  : sorted nil
+| sorted_one  : forall n : nat, sorted (cons n nil)
+| sorted_cons : forall x xs n, n <= x -> sorted (cons x xs) -> sorted (n :: x :: xs).
 
 (* 2. Napisz funkcję sortowania przez wstawianie o następującej specyfikacji: *)
 
 Require Import Permutation.
 
-(* Definition insertion_sort : forall l : list nat, 
-  {l' : list nat | Permutation l l' /\ sorted l'}. *)
+Print Permutation.
+Search (forall x, Permutation x x).
+(* Inductive Permutation (A : Type) : list A ⟶ list A ⟶ Prop ≜
+    perm_nil : Permutation nil nil
+  | perm_skip : ∀ (x : A) (l l' : list A),
+                Permutation l l' ⟶ Permutation (x :: l) (x :: l')
+  | perm_swap : ∀ (x y : A) (l : list A),
+                Permutation (y :: x :: l) (x :: y :: l)
+  | perm_trans : ∀ l l' l'' : list A,
+                 Permutation l l' ⟶ Permutation l' l'' ⟶ Permutation l l'' *)
+
+Search ((~ (_ <= _)) = _ > _).
+
+Search (_ <= _ \/ _ < _).
+Search (forall a b, {a <= b} + {a > b}).
+Search (Permutation (_ :: nil) (_ :: _) -> _).
+
+Lemma is_le (a b : nat) : {a <= b} + {a > b}.
+Proof.
+  apply Compare_dec.le_gt_dec.
+  (* assert (a <= b \/ b < a).
+  apply Nat.le_gt_cases.
+  destruct H.
+
+  decide equality.
+  assert (a > b = ~ (a <= b)).
+  
+  unfold gt.
+  change ({a <= b} + {~ (a <= b)}). *)
+Qed.
+
+Lemma sort_impl_hd_le : forall xs x a, sorted (x :: xs) -> In a xs -> x <= a.
+Proof.
+  induction xs; intros.
+  - cbn in H0. inversion H0.
+  - cbn in H0. destruct H0; subst.
+    + inversion H. subst. assumption.
+    + apply IHxs.
+      inversion H. clear H. subst. 
+      inversion H5. clear H5. subst.
+      inversion H0. subst.
+      apply sorted_cons; auto. apply Nat.le_trans with (m := a); auto.
+      assumption.
+Qed.
+
+Lemma perm_in : forall {A : Type} l x xs, @Permutation A l (x :: xs) -> In x l.
+Proof.
+  intros.
+  dependent induction H.
+  - cbn. auto.
+  - cbn. auto.
+  - assert (x :: xs = x :: xs). auto.
+    apply IHPermutation2 in H1.
+    apply Permutation_sym in H.
+    apply Permutation_in with (l := l'); auto.
+Qed.
+
+Lemma ins_aux : forall x xs 
+  , sorted (x :: xs)
+  -> forall n a l, sorted (a :: l)
+  -> x < n
+  -> Permutation (n :: xs) (a :: l)
+  -> x <= a.
+Proof.
+  intros.
+  apply perm_in in H2. cbn in H2. destruct H2.
+  - rewrite <- H2. auto with arith.
+  - apply sort_impl_hd_le with (xs := xs); auto.
+Qed.
+
+Definition insert : forall (n : nat) (l : list nat) (s : sorted l),
+  {l' : list nat | Permutation (n :: l) l' /\ sorted l'}.
+refine (
+  fix ins (n : nat) (l : list nat) : 
+    sorted l -> {l' : list nat | Permutation (n :: l) l' /\ sorted l'} :=
+    match l with
+    | nil => fun s => exist _ (n :: nil) _
+    | x :: xs => fun s =>
+      match is_le n x with
+      | left  n_le_x => exist _ (n :: x :: xs) _
+      | right x_gt_n => 
+        match ins n xs _ with
+        | exist _ nxs p => exist _ (x :: nxs) _
+        end
+      end  
+    end
+).
+- split. apply perm_skip. apply perm_nil. apply sorted_one.
+- split. apply Permutation_refl. apply sorted_cons; auto.
+- inversion s. clear s. subst. apply sorted_nil. assumption.
+- split; cbn in p; destruct p.
+  + assert (Permutation (n :: x :: xs) (x :: n :: xs)).
+    apply perm_swap. apply perm_skip with (x := x) in H.
+    apply perm_trans with (l' := x :: n :: xs); assumption.
+  + induction nxs.
+    * apply sorted_one.
+    * apply sorted_cons.
+      apply ins_aux with (xs := xs) (n := n) (l := nxs); auto.
+      auto.
+
+Fixpoint insert (n : nat) (l : list nat)
+  (e1 : {l' : list nat | Permutation l l' /\ sorted l'})
+  : {t : list nat | Permutation (n :: l) t /\ sorted t}.
+  (* match e1 with
+  | exist _ nil _ => exist _ (n :: nil) _
+  | exist _ (cons x xs) _ => 
+    match (is_le n x) with
+    | left n_le_x => exist _ (n :: x :: xs) _
+    | right _ => 
+      match insert n xs _ with
+      | exist _ xs' _ => exist _ (x :: xs') _ 
+      end
+    end
+  end. *)
+
+(* refine (
+  match e1 with
+  | exist _ l' p =>
+    match l' with
+    | nil => exist _ (n :: nil) _
+    | cons x xs => 
+      match (is_le n x) with
+      | left n_le_x => exist _ (n :: x :: xs) _
+      | right x_gt_n => 
+        match insert n xs _ with
+        | exist _ xs' _ => exist _ (x :: xs') _ 
+        end
+      end
+    end
+  end
+); cbn in p.
+- admit.
+- admit.  *)
+
+(* induction e1. destruct p. rename x into l'.
+induction l'.
+- exists (n :: nil). split. auto. apply sorted_one.
+- dependent destruction H. rename n0 into x.
+  induction (is_le n x); admit.
+- 
+  + refine (exist _ (n :: x :: l') _).
+    split. auto. apply sorted_cons; auto.
+  + induction (insert n l' _). *)
+
+induction e1. destruct p. rename x into l'.
+induction l'.
+- exists (n :: nil). split. auto. apply sorted_one.
+- rename a into x.
+  induction (is_le n x).
+  + refine (exist _ (n :: x :: l') _).
+    split. auto. apply sorted_cons; auto.
+  + induction (insert n l' _).
+
+Fixpoint insertion_sort (l : list nat) :
+  {l' : list nat | Permutation l l' /\ sorted l'} :=
+  match l with
+  | nil => exist _ nil _
+  | cons x xs => insert x (insertion_sort xs)
+  end.
 
 (*** Zadanie 3 - 8p *)
 
