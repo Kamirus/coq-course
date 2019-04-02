39d9956baa1419ec01b151e0ab018c24e1f72f26
Kamirus <listopadkamil@gmail.com>
2019-04-02 21:19:14 +0200

    l5 style

--- l5.v
+++ l5.v
@@ -15,8 +15,8 @@ Fixpoint inject (A : Type) (ls : list A) : llist (length ls) :=
   match ls with
   | nil => lnil
   | x :: xs => lcons _ x (inject A xs)
-  end.
-
+  end
+.
 Require Program.
 
 Obligation Tactic := eauto.
@@ -112,8 +112,8 @@ Fixpoint llcoerce {A : Type} (n : nat) (l : @llist A n) :
   match l with
   | lnil => lnil
   | lcons n x xs => lcons _ x (llcoerce n xs)
-  end.
-
+  end
+.
 Lemma in_o_un_id : forall A n l, inject A (unject A n l) = llcoerce n l.
 Proof.
   intros. induction l; cbn; auto.
@@ -132,17 +132,6 @@ Inductive sorted : list nat -> Prop :=
 (* 2. Napisz funkcję sortowania przez wstawianie o następującej specyfikacji: *)
 
 Require Import Permutation.
-
-Print Permutation.
-Search (forall x, Permutation x x).
-(* Inductive Permutation (A : Type) : list A ⟶ list A ⟶ Prop ≜
-    perm_nil : Permutation nil nil
-  | perm_skip : ∀ (x : A) (l l' : list A),
-                Permutation l l' ⟶ Permutation (x :: l) (x :: l')
-  | perm_swap : ∀ (x y : A) (l : list A),
-                Permutation (y :: x :: l) (x :: y :: l)
-  | perm_trans : ∀ l l' l'' : list A,
-                 Permutation l l' ⟶ Permutation l' l'' ⟶ Permutation l l'' *)
 
 Search ((~ (_ <= _)) = _ > _).
 
@@ -203,55 +192,6 @@ Proof.
   - apply sort_impl_hd_le with (xs := xs); auto.
   Qed.
 
-Definition insert : forall (n : nat) (l : list nat) (s : sorted l),
-  {l' : list nat | Permutation (n :: l) l' /\ sorted l'}.
-  refine (
-    fix ins (n : nat) (l : list nat) : 
-      sorted l -> {l' : list nat | Permutation (n :: l) l' /\ sorted l'} :=
-      match l with
-      | nil => fun s => exist _ (n :: nil) _
-      | x :: xs => fun s =>
-        match is_le n x with
-        | left  n_le_x => exist _ (n :: x :: xs) _
-        | right x_gt_n => 
-          match ins n xs _ with
-          | exist _ nxs p => exist _ (x :: nxs) _
-          end
-        end  
-      end
-  ).
-  - split. apply perm_skip. apply perm_nil. apply sorted_one.
-  - split. apply Permutation_refl. apply sorted_cons; auto.
-  - inversion s. clear s. subst. apply sorted_nil. assumption.
-  - split; cbn in p; destruct p.
-    + assert (Permutation (n :: x :: xs) (x :: n :: xs)).
-      apply perm_swap. apply perm_skip with (x := x) in H.
-      apply perm_trans with (l' := x :: n :: xs); assumption.
-    + induction nxs.
-      * apply sorted_one.
-      * apply sorted_cons.
-        apply ins_aux with (xs := xs) (n := n) (l := nxs); auto.
-        auto.
-Defined.
-
-Lemma snd : forall P Q, P /\ Q -> Q.
-Proof.
-  intros. destruct H. assumption.
-Defined.
-
-
-(* Definition insertion_sort (l : list nat) : {l' : list nat | Permutation l l' /\ sorted l'}.
-refine (
-  fix ins_sort (l : list nat) : {l' : list nat | Permutation l l' /\ sorted l'} :=
-    match l return {l' : list nat | Permutation l l' /\ sorted l'} with
-    | nil => exist _ nil _
-    | cons x xs =>
-      match ins_sort xs as f return {l' : list nat | Permutation (x :: proj1_sig f) l' /\ sorted l'} with
-      | exist _ xs' p => insert x xs' (fst p)
-      end
-    end
-). *)
-
 Fixpoint ins (n : nat) (l : list nat) :=
   match l with
   | nil => cons n nil
@@ -260,14 +200,14 @@ Fixpoint ins (n : nat) (l : list nat) :=
     | left  n_le_x => n :: l
     | right n_gt_x => x :: ins n xs
     end
-  end.
-
+  end
+.
 Fixpoint ins_sort (l : list nat) :=
   match l with
   | nil => nil
   | x :: xs => ins x (ins_sort xs)
-  end.
-
+  end
+.
 Lemma perm_ins : forall n l, Permutation (n :: l) (ins n l).
 Proof.
   intros. induction l.
@@ -315,8 +255,8 @@ Proof.
 
 Definition insertion_sort (l : list nat) : 
   {l' : list nat | Permutation l l' /\ sorted l'} :=
-  exist _ (ins_sort l) (conj (perm_ins_sort l) (sorted_ins_sort l)).
-
+  exist _ (ins_sort l) (conj (perm_ins_sort l) (sorted_ins_sort l))
+.
 (*** Zadanie 3 - 8p *)
 
 (* Rozwiąż poprzednie zadanie używając typu danych sorted_list indeksowanych
